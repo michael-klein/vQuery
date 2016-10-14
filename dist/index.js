518deebf0c7c604ea1640e5a972beea8aef49894
Michael Klein <m.klein@wondermags.com>
2016-10-14 22:20:25 +0200

    performance (reverted from commit c53c33ab9754107f27e6dbbfdb895eb0605e615d)

--- dist/index.js
+++ dist/index.js
@@ -8872,9 +8872,8 @@ vQuery.getDOM = function() {
 
 },{"./diff.js":61,"./render.js":63,"./selectorEngine.js":64,"./vDOM.js":65,"./virtualQuery.js":66,"clone":27,"domready":30}],63:[function(require,module,exports){
 
-var vDOM = require('./vDOM.js'),
-    docFrag = document.createDocumentFragment(),
-    rendering = false;
+var vDOM = require('./vDOM.js');
+var rendering = false;
 var decodeEntities = (function() {
     // this prevents any overhead from creating the object each time
     var element = document.createElement('div');
@@ -8920,51 +8919,6 @@ function handleListeners(domNode, listeners) {
         }
     }
 }
-function depth(node) {
-    var d = 0;
-    while (node = node.parentNode, node.localName !== "body") {
-        d++;
-    }
-    return d;
-}
-function isDescendant(parent, child) {
-    var node = child.parentNode;
-    while (node != null) {
-        if (node == parent) {
-            return true;
-        }
-        node = node.parentNode;
-    }
-    return false;
-}
-var topDepth = Number.MAX_VALUE;
-var topNodes = [];
-
-function setTop(node, s) {
-    for (i=0; i<topNodes.length; i++) {
-        if (isDescendant(topNodes[i].n, node))
-            return;
-        if (isDescendant(node, topNodes[i].n)) {
-            topNodes[i] = {
-                s: s,
-                n: node
-            };
-            return;
-        }
-        if (topNodes[i].s === s) {
-            topNodes[i] = {
-                s: s,
-                n: node
-            };
-            return;
-        }
-    }
-
-    topNodes.push({
-        s: s,
-        n: node
-    });
-}
 
 
 module.exports = {
@@ -8973,51 +8927,40 @@ module.exports = {
     },
     render: function(ops,node) {
         var t = new Date().getTime();
-        topNodes = []
         console.log("rendering start", t, ops);
         rendering = true;
-        var root = node ? node : document.querySelector("html"),
-            buffer = docFrag.appendChild(root.cloneNode(true));
+        var root = node ? node : document;
         for (var i = 0; i < ops.length; i++) {
             var op = ops[i];
             switch (op.t) {
                 case "remove":
-                    var node = buffer.querySelector(op.p),
-                        parent = node.parentNode ? node.parentNode : buffer;
-                    setTop(parent, op.p);
+                    var node = root.querySelector(op.p),
+                        parent = node.parentNode ? node.parentNode : root;
                     parent.removeChild(node);
                     if (op.hl)
                         handleListeners(node, op.l);
                     break;
                 case "addNode":
                     var newNode = createNode(op.n),
-                        parent = op.p.length > 0 ? buffer.querySelector(op.p) : buffer;
+                        parent = op.p.length > 0 ? root.querySelector(op.p) : root;
                     parent.appendChild(newNode);
                     if (op.hl)
                         handleListeners(newNode, op.l);
-                    setTop(parent, op.p);
                     break;
                 case "replace":
                     var newNode = createNode(op.n),
-                        node = buffer.querySelector(op.p),
-                        parent = node.parentNode ? node.parentNode : buffer;
+                        node = root.querySelector(op.p),
+                        parent = node.parentNode ? node.parentNode : root;
                     parent.replaceChild(newNode, node);
                     if (op.hl)
                         handleListeners(newNode, op.l);
-                    setTop(parent, op.p);
                     break;
                 case "attrChanged":
-                    var n = buffer.querySelector(op.n);
-                    n.setAttribute(op.a, op.v);
-                    setTop(n, op.n)
+                    root.querySelector(op.n).setAttribute(op.a, op.v);
                     break;
             }
         }
-        for (i=0; i<topNodes.length; i++) {
-            root.querySelector(topNodes[i].s).parentNode.replaceChild(topNodes[i].n, root.querySelector(topNodes[i].s));
-        }
-        docFrag.removeChild(buffer);
-        console.log("rendering stop", (new Date().getTime() - t) / 1000, topNodes);
+        console.log("rendering stop", (new Date().getTime() - t) / 1000);
         rendering = false;
     }
 }

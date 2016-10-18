cfa6254cef82765f192fbfc24e74368b1eadbc23
Michael Klein <m.klein@wondermags.com>
2016-10-18 13:20:18 +0200

    test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
+/**
+ * Check if it works in the node environment
+ */
+
 var $ = require('../src/index.js');
 
 $.load('<body><div id="test" class="test"><div><h1><p class="hello"></p><p>asd</p></h1></div></div><div id="append">trollol</div><div id="attr" align="left"></div></body>');
